0af2cba66c732898a45cec19067eefb15a414eec
abkaluga <a.b.kaluga@gmail.com>
2015-01-28 21:30:06 +0100

    SMS box added, Sms list made, File.make_sms bugfix

--- o.ml
+++ o.ml
@@ -23,8 +23,8 @@ module Sms = struct
 	let cmp_addr a b = 
 		compare (List.assoc "address" a) (List.assoc "address" b)
 
-	let compare (a:t) (b:t) =
-		match cmp_date a b with
+	let compare a b =
+		match (cmp_date a b) with
 		| 0 -> cmp_addr a b
 		| c -> c
 end
@@ -54,21 +54,20 @@ module File = struct
             ) friends parsedFile
 
 	let make_smss parsedFile = 
-		let smss =  SS.empty in
-			Xml.fold (fun aux e ->
-				SS.add 
-				([("name",(Xml.attrib  e "contact_name"));
+			Xml.fold (fun aux e  -> 
+				SS.add ([("name",(Xml.attrib  e "contact_name"));
 				("body",(Xml.attrib  e "body"));
+				("type",(Xml.attrib  e "type"));
 				("rdate",(Xml.attrib  e "readable_date"));
 				("date",(Xml.attrib  e "date"));
 				("address",(clear_number (Xml.attrib e "address")));
-				("r",(Xml.attrib  e "read"))]) smss ) smss (parsedFile)
+				("r",(Xml.attrib  e "read"))]) aux ) SS.empty (parsedFile)
 	
 
 
 	let default = [(-10,Sms.cmp_name); (5,Sms.cmp_date)]
 
-	let impor li a b = 
+	let important li a b = 
 		let rec inner aux li' = 
 		match li' with
 		| [] -> aux
@@ -83,7 +82,7 @@ end
 
 module Gui = struct
   let opened = ref "test.xml"
-  let test = SS.min_elt (File.make_smss (File.parse_xml  !opened))
+  let test = (File.make_smss (File.parse_xml  !opened))
 
 
 let init = GMain.Main.init ()
@@ -94,16 +93,18 @@ let file_ok_sel filew () =
   print_endline filew#filename;
   flush stdout
 
-let toSelect ali = 
-	"Name:\t\t"^(List.assoc "name" ali)
-	^"\nDate:\t\t"^(List.assoc "rdate" ali)
-	^"\nSeen:\t\t"^(List.assoc "r" ali)
-	^"\nBody:\t\t"^(List.assoc "body" ali)
-let toBox ali =
-	(List.assoc "name" ali)
-	^"\t"^(List.assoc "rdate" ali)
-	^"\t"^(List.assoc "r" ali)
-	^"\t"^(List.assoc "body" ali)
+let toBox sms box =
+	let nameLabel =GMisc.label ~text: (List.assoc "name" sms) ~width:100 ~line_wrap:true ~justify:`LEFT
+    		~packing:box#add () in 
+    let rdateLabel =GMisc.label ~text: (List.assoc "rdate" sms) ~width:150
+    		~packing:box#add () in 
+    let rLabel =GMisc.label ~text: (List.assoc "r" sms) ~width:10 ~justify:`FILL
+    		~packing:box#add () in 
+    let wayLabel =GMisc.label ~text: (List.assoc "type" sms) ~width:10 ~justify:`FILL
+    		~packing:box#add () in 
+
+    let bodyLabel =GMisc.label ~text: (List.assoc "body" sms) ~width:800 ~line_wrap:true ~justify:`FILL
+    		~packing:box#add () in ()
 
 let file_selection () =
   (* Create a new file selection widget; set default filename *)
@@ -115,22 +116,39 @@ let file_selection () =
   GMain.Main.main ()
   
 
-  let window = GWindow.window ~title:"Wymyśl nazwę" ~width:300 ~height:300 ~border_width:10 ()
+  let window = GWindow.window ~title:"Wymyśl nazwę" ~width:1070 ~height:300 ~border_width:10 ()
 
   let mainbox = GPack.vbox ~packing:window#add ()
-  let frame = GBin.frame ~label:"SMS details" ~label_xalign:1.0  ~packing:mainbox#add ()
-  let smslabel = GMisc.label ~text:"sometest " ~packing:frame#add ()
+  let infobox = GPack.hbox ~packing:mainbox#add () 
+  let frame = GBin.frame ~label:"SMS details" ~label_xalign:1.0 ~width:300  ~packing:infobox#add ()
+  let smsBox = GPack.hbox ~packing: frame#add()
+  let smsLabels = 
+  		let helpBox = GPack.vbox ~packing:smsBox#add () in
+  		List.iter (fun str -> GMisc.label ~text: str ~width:100 ~packing: helpBox#add () ;()) ["Name"; "Date";"Seen";"Recived/Sended(1/2)";"Body"]
 
-  let event_box = GBin.event_box ~packing:mainbox#add ()
-  let label = GMisc.label ~text: (toBox test) ~line_wrap:true ~justify:`FILL
-    ~packing:event_box#add () 
+  let selected_sms = GPack.vbox ~width:500 ~packing:  smsBox#add ()
+  
+  let scrolled_window = GBin.scrolled_window ~border_width:10
+   	 	~hpolicy:`AUTOMATIC ~vpolicy:`AUTOMATIC ~packing:mainbox#add ()
+  let sms_list = GPack.vbox ~packing: scrolled_window#add_with_viewport() 
+
+  let create_line sms = 
+  			let event_box = GBin.event_box ~packing:sms_list#add() in 
+  				let line = GPack.hbox ~border_width:5 ~width:1100 ~packing:event_box#add () in
+  			toBox sms line;
+    		event_box#event#add [`BUTTON_PRESS];
+  			event_box#event#connect#button_press ~callback: 
+  						(fun env -> List.iter 
+  							(fun e -> selected_sms#remove e) 
+  								selected_sms#all_children; 
+  							List.iter (fun str -> GMisc.label ~text: (List.assoc str sms) ~width:600 ~line_wrap:true ~packing: selected_sms#add () ;()) 
+  								["name"; "rdate";"r";"type";"body"];
+  					true );
+  			event_box#misc#realize ()
 
   let main () =
- (* 	button#connect#clicked ~callback: (fun () -> smslabel#set_text (update test)); *)
- 	event_box#event#add [`BUTTON_PRESS];
-  	event_box#event#connect#button_press ~callback: (fun env -> smslabel#set_text (toSelect test);true );
-  	event_box#misc#realize ();
-  	Gdk.Window.set_cursor event_box#misc#window (Gdk.Cursor.create `HAND1);
+  	List.iter (fun e -> sms_list#remove e) sms_list#all_children;
+ 	SS.iter create_line test;
     window#connect#destroy ~callback:GMain.Main.quit;
     window#show ();
     GMain.Main.main ()
